580ed1f77651c4e8eac6bac4763a1bf0f442a37a
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 19:13:34 +0300

    Helper en ES6

--- Helpers/helper.js
+++ Helpers/helper.js
@@ -1,8 +1,3 @@
-const success = (message, data) => {
-    return {
-        message: message,
-        data: data
-    }
-}
-
-exports.success;
+exports.success = (message, data) => {
+    return {message, data}
+};
