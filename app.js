e1be0d21374622135a4ab557477b1ae51820234e
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 18:57:44 +0300

    Mise en place de endpoint /

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+let pokemons = require('./mocks/mock-pokemon');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Express !'));
+app.get('/api/pokemons', (req, res) => {
+    const nb =  pokemons.length;
+    res.send(`Il y a ${nb} pokemons`);
+});
 app.get('/api/pokemons/:id', (req,res) => {
-    const id = req.params.id;
-    res.send("Vous avez demandé le paramètre n°"+id);
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+    res.send(`Vous avez demandé le pokemon ${pokemon.name}`);
 });
 
 app.listen(PORT, () => console.log(`Server running in : http://localhost:${PORT}`));
