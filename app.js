69fe23ab0b3c971e6942c77b733f09049507114b
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 22:41:44 +0300

    Ajout de route delete et connecter au app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ require('./src/routes/findAllPokemons')(app);
 require('./src/routes/findPokemonByPk')(app);
 require('./src/routes/createPokemon')(app);
 require('./src/routes/updatePokemon')(app);
+require('./src/routes/deletePokemon')(app);
 
 app.listen(PORT, () =>
   console.log(`Server running in : http://localhost:${PORT}`)
