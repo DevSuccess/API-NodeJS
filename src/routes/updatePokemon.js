79f76b56ffe64998fa827844a4889de2c29be78d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 23:24:45 +0300

    Rendre le promise catch

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -7,19 +7,14 @@ module.exports = (app) => {
       where: { id: id },
     })
       .then((_) => {
-        Pokemon.findByPk(id)
-          .then((pokemon) => {
-            if (!pokemon === null) {
-              const message = `La pokemon demander n'existe pas !`;
-              return res.status(404).json({ message });
-            }
-            const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
-            res.json({ message, data: pokemon });
-          })
-          .catch((error) => {
-            const message = `La pokemon n'a pas pu être modifier; Réessayer dans quelques instants.`;
-            res.status(500).json({ message, data: error });
-          });
+        return Pokemon.findByPk(id).then((pokemon) => {
+          if (!pokemon === null) {
+            const message = `La pokemon demander n'existe pas !`;
+            return res.status(404).json({ message });
+          }
+          const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
+          res.json({ message, data: pokemon });
+        });
       })
       .catch((error) => {
         const message = `La pokemon n'a pas pu être modifier; Réessayer dans quelques instants.`;
