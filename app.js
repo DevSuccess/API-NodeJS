bf7a2e2396e60f60c73e94f0c9b4ac665a72cb44
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 18:40:57 +0300

    Ajout de recuperation de paramatre

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Express !'));
-app.get('/api/pokemons/1', (req,res) => res.send("Hello Bulbizar"));
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = req.params.id;
+    res.send("Vous avez demandé le paramètre n°"+id);
+});
 
 app.listen(PORT, () => console.log(`Server running in : http://localhost:${PORT}`));
