01b6ae60007750d5740f617d76adf9939450c44e
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 18:26:45 +0300

    Mise en place de Hello Express

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log();
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello, Express !'));
+
+app.listen(port, () => console.log("Server running in : http://localhost:"+port));
