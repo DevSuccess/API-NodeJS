083e007363ffd3e3eb1d7644ae728fad2f38dc4d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 19:38:55 +0300

    installer et mise en place du middleware morgan

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const {success} = require("./helpers/helper");
 let pokemons = require('./mocks/mock-pokemon');
 
 const app = express();
 const PORT = 3000;
 
-app.use((req, res, next) => {
-    console.log(`URL: ${req.url}`);
-    next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.send('Hello, Express !'));
 app.get('/api/pokemons', (req, res) => {
