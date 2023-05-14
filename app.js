212d8e5e4eaab78c936dfcd44cebe92a90a69e00
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-14 18:36:32 +0300

    Mise en place nouveau endpoint

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Express !'));
+app.get('/api/pokemons/1', (req,res) => res.send("Hello Bulbizar"));
 
-app.listen(port, () => console.log("Server running in : http://localhost:"+port));
+app.listen(PORT, () => console.log(`Server running in : http://localhost:${PORT}`));
