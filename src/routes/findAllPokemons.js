0580a5071ae559b8bad96b4b542102c592cc9afa
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-15 07:51:38 +0300

    Point de terminaison limit par l'utilisateur

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -5,18 +5,25 @@ module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
     if (req.query.name) {
       const name = req.query.name;
+      const limit = parseInt(req.query.limit) || 5;
+
+      if(name.length < 2){
+        const message = `Le terme de recherche doit être au moins 2 caractères.`;
+        return res.status(400).json({message});
+      }
+
       return Pokemon.findAndCountAll({
         where: {
-          name: { [Op.like]: `%${name}%` }
+          name: { [Op.like]: `%${name}%` },
         },
-        order: ['name'],
-        limit: 5
-      }).then(({count, rows}) => {
+        order: ["name"],
+        limit: limit
+      }).then(({ count, rows }) => {
         const message = `Il y a ${count} pokémons qui correspond au terme rechercher ${name}`;
         res.json({ message, data: rows });
       });
     } else {
-      Pokemon.findAll({order: ['name']})
+      Pokemon.findAll({ order: ["name"] })
         .then((pokemons) => {
           const message = "La liste des pokémons a bien été récupérée.";
           res.json({ message, data: pokemons });
