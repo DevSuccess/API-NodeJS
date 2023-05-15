cef52ba2eec145120b6ea4f6502c794e856c9cd4
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-15 07:32:51 +0300

    Fonctionalité de recherche

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,28 @@
 const { Pokemon } = require("../data/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
-    Pokemon.findAll()
-      .then((pokemons) => {
-        const message = "La liste des pokémons a bien été récupérée.";
+    if (req.query.name) {
+      const name = req.query.name;
+      return Pokemon.findAll({
+        where: {
+          name: { [Op.like]: `%${name}%` }
+        },
+      }).then((pokemons) => {
+        const message = `Il y a ${pokemons.length} pokémons qui correspond au terme rechercher ${name}`;
         res.json({ message, data: pokemons });
-      })
-      .catch((error) => {
-        const message = `La liste des pokemons n'a pas pu être récupérée; Réessayer dans quelques instants.`;
-        res.status(500).json({ message, data: error });
       });
+    } else {
+      Pokemon.findAll()
+        .then((pokemons) => {
+          const message = "La liste des pokémons a bien été récupérée.";
+          res.json({ message, data: pokemons });
+        })
+        .catch((error) => {
+          const message = `La liste des pokemons n'a pas pu être récupérée; Réessayer dans quelques instants.`;
+          res.status(500).json({ message, data: error });
+        });
+    }
   });
 };
