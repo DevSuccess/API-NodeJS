1671ddcb527ec3aef26c0afac9c75f98da843490
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-15 10:30:06 +0300

    Mise en place de variable d'environnement dynamic

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const sequelize = require("./src/data/sequelize");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Les middlewares :
 app
